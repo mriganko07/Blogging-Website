a2bb323df327338ac9779bb28185fe775096bef0
mriganko07 <mrigankaadhikary35@gmail.com>
2024-11-07 12:02:04 +0530

    COMMUNITY DB

---
+++ database/migrations/2024_11_07_055913_create_communities_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('communities', function (Blueprint $table) {
+            $table->id('community_id');
+            $table->string('community_name');
+            $table->string('community_description');
+            $table->string('community_pic');
+            $table->string('community_coverpic');
+            $table->integer('community_total_member');
+            $table->integer('community_total_posts');
+            $table->enum('category',["games", "technologies", "movies", "travel", "music", "education", "sport", "news_politics", "business_finance"])->nullable();
+            $table->integer('community_suspend');
+            // $table->foreign('user_id')->references('user_id')->on('users')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('communities');
+    }
+};
